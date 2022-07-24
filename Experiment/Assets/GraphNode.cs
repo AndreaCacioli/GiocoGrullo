56ae11b1f596a38ad55c86d15c64103aee9bcbe1
Andrea Cacioli <andrea.cacioli@edu.unito.it>
2022-07-24 11:38:00 +0200

    Initial Commit

---
+++ Experiment/Assets/GraphNode.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
+public class GraphNode
+{
+    private TileBase tileBase;
+    private List<GraphNode> neighbours; 
+
+    public GraphNode(TileBase t)
+    {
+        this.tileBase = t;
+        neighbours = new List<GraphNode>();
+    }
+    public void addNeighbour(GraphNode graphNode) {
+        if(!neighbours.Contains(graphNode))neighbours.Add(graphNode);
+    }
+
+    public List<GraphNode> getNeighbours() { return neighbours; }
+
+
+    public TileBase getTileBase() { return tileBase; }
+}
